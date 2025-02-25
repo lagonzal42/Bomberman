cba5f57475ed8d9803f4b09c534503461875eed7
LUC1A05 <ldel-rio@student.42urduliz.com>
2025-02-25 17:44:57 +0100

    matrize klasea hasita

--- src/eredua/Matrizea.java
+++ src/eredua/Matrizea.java
@@ -1,5 +1,20 @@
 package eredua;
 
-public class Matrizea {
+import java.util.Observable;
+import java.util.Random;
 
+public class Matrizea extends Observable{
+	
+	private static Matrizea nMatrizea = new Matrizea();
+	private Objektu[][] zerrenda; //objektu klasea aldatu behar da.
+	
+	private Matrizea() {
+		//TODO
+	}
+	
+	private void matrizeaSortu() {
+		Random r = new Random();
+		zerrenda = new Objektu[17][11];
+		for(zerrenda[1].length)
+	}
 }
