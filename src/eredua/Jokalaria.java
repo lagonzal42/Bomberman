728c8fe8d2759e21af400a2fc6762f764cda17b7
LUC1A05 <ldel-rio@student.42urduliz.com>
2025-03-01 17:47:07 +0100

    posizioak zuzenduta

--- src/eredua/Jokalaria.java
+++ src/eredua/Jokalaria.java
@@ -9,28 +9,28 @@ public abstract class Jokalaria extends Gelaxka{
 	
 	
 	public void mugituGora(){
-		if (Matrizea.getMatrizea().getGelaxka(xPos, yPos-1) instanceof Hutsik){
+		if (Matrizea.getMatrizea().getGelaxka(yPos-1, xPos) instanceof Hutsik){
 			Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos, yPos-1);
 			this.yPos = this.yPos -1;
 		}
 	}
 	
 	public void mugituBehera(){
-		if (Matrizea.getMatrizea().getGelaxka(xPos, yPos+1) instanceof Hutsik){
+		if (Matrizea.getMatrizea().getGelaxka(yPos+1, xPos) instanceof Hutsik){
 			Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos, yPos+1);
 			this.yPos = this.yPos +1;
 		}
 	}
 	
 	public void mugituEzkerra(){
-		if (Matrizea.getMatrizea().getGelaxka(xPos-1, yPos) instanceof Hutsik){
+		if (Matrizea.getMatrizea().getGelaxka(yPos, xPos-1) instanceof Hutsik){
 			Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos-1, yPos);
 			this.xPos = this.xPos -1;
 		}
 	}
 	
 	public void mugituEskuma() {
-		if (Matrizea.getMatrizea().getGelaxka(xPos+1, yPos) instanceof Hutsik){
+		if (Matrizea.getMatrizea().getGelaxka(yPos, xPos+1) instanceof Hutsik){
 			Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos+1, yPos);
 			this.xPos = this.xPos +1;
 		}
