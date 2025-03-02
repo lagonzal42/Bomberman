1399295275f011321151f24bc44858d0e322a142
LUC1A05 <ldel-rio@student.42urduliz.com>
2025-03-02 23:30:05 +0100

    posizioa aldatzen ez denean jokalaria mugitzeko aldaketak

--- src/eredua/Jokalaria.java
+++ src/eredua/Jokalaria.java
@@ -18,47 +18,47 @@ public abstract class Jokalaria{
 	
 	
 	public void mugituGora(){
+		this.setAzkenMugi(Mugimendu.GORA);
 		//this.setIrudia("whiteup1.png");
 		if (Matrizea.getMatrizea().getGelaxka(yPos-1, xPos) instanceof Hutsik){
 			//Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos, yPos-1);
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).setJokalaria(false);
 			Matrizea.getMatrizea().getGelaxka(yPos - 1, xPos).setJokalaria(true);
-			this.setAzkenMugi(Mugimendu.GORA);
 			this.yPos = this.yPos -1;
 		}
 	}
 	
 	public void mugituBehera(){
+		this.setAzkenMugi(Mugimendu.BEHERA);
 		//this.setIrudia("whitedown1.png");
 		if (Matrizea.getMatrizea().getGelaxka(yPos+1, xPos) instanceof Hutsik){
 			//Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos, yPos+1);
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).setJokalaria(false);
 			Matrizea.getMatrizea().getGelaxka(yPos + 1, xPos).setJokalaria(true);
-			this.setAzkenMugi(Mugimendu.BEHERA);
 			this.yPos = this.yPos +1;
 		}
 	}
 	
 	public void mugituEzkerra(){
+		this.setAzkenMugi(Mugimendu.EZKER);
 		//this.setIrudia("whiteleft1.png");
 		if (Matrizea.getMatrizea().getGelaxka(yPos, xPos-1) instanceof Hutsik){
 			//Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos-1, yPos);
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).setJokalaria(false);
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos - 1).setJokalaria(true);
-			this.setAzkenMugi(Mugimendu.EZKER);
 			
 			this.xPos = this.xPos -1;
 		}
 	}
 	
 	public void mugituEskuma() {
+		this.setAzkenMugi(Mugimendu.ESKUIN);
 		//this.setIrudia("whiteright1.png");
 		if (Matrizea.getMatrizea().getGelaxka(yPos, xPos+1) instanceof Hutsik){
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).setJokalaria(false);
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos + 1).setJokalaria(true);
 			//Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos+1, yPos);
 			this.xPos = this.xPos +1;
-			this.setAzkenMugi(Mugimendu.ESKUIN);
 		}
 	}
 	
