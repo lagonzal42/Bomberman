ff3d35ca2a4767a065e1ba9cfc7a0d54115494df
Asier <asier08032010@gmail.com>
2025-04-02 22:00:57 +0200

    Jokalaria -> Etsaia (HILDA)

--- src/eredua/Jokalaria.java
+++ src/eredua/Jokalaria.java
@@ -33,7 +33,8 @@ public abstract class Jokalaria{
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).eguneratuGelaxka();
 			Matrizea.getMatrizea().getGelaxka(yPos - 1, xPos).setJokalaria(this);
 			this.yPos = this.yPos -1;
-			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true) {
+			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true || 
+					Matrizea.getMatrizea().getGelaxka(yPos,xPos).getEtsaia() != null) {
 				this.hil = true;
 				this.setAzkenMugi(Mugimendu.HILDA);
 			}
@@ -50,11 +51,11 @@ public abstract class Jokalaria{
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).eguneratuGelaxka();
 			Matrizea.getMatrizea().getGelaxka(yPos + 1, xPos).setJokalaria(this);
 			this.yPos = this.yPos +1;
-			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true) {
+			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true || 
+					Matrizea.getMatrizea().getGelaxka(yPos,xPos).getEtsaia() != null) {
 				this.hil = true;
 				this.setAzkenMugi(Mugimendu.HILDA);
 			}
-			//Matrizea.getMatrizea().getGelaxka(yPos, xPos).eguneratuGelaxka();
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).eguneratuGelaxka();
 		}
 	}
@@ -70,7 +71,8 @@ public abstract class Jokalaria{
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos).eguneratuGelaxka();
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos - 1).setJokalaria(this);
 			this.xPos = this.xPos -1;
-			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true) {
+			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true || 
+					Matrizea.getMatrizea().getGelaxka(yPos,xPos).getEtsaia() != null) {
 				this.hil = true;
 				this.setAzkenMugi(Mugimendu.HILDA);
 			}
@@ -89,7 +91,8 @@ public abstract class Jokalaria{
 			Matrizea.getMatrizea().getGelaxka(yPos, xPos + 1).setJokalaria(this);
 			//Matrizea.getMatrizea().aldatuPos(xPos, yPos, xPos+1, yPos);
 			this.xPos = this.xPos +1;
-			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true) {
+			if (Matrizea.getMatrizea().getGelaxka(yPos, xPos).getSua() == true || 
+					Matrizea.getMatrizea().getGelaxka(yPos,xPos).getEtsaia() != null) {
 				this.hil = true;
 				this.setAzkenMugi(Mugimendu.HILDA);
 			}
