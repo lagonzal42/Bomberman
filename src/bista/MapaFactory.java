bac4bb68b142011f27639204fe0fae0ea5b47b02
Ainhoa <ainhoatomas@gmail.com>
2025-04-03 10:48:30 +0000

    MapaFactory klasea

---
+++ src/bista/MapaFactory.java
@@ -0,0 +1,17 @@
+package bista;
+
+public class MapaFactory {
+	private static MapaFactory nMP;
+	private MapaFactory() {}
+	
+	public MapaFactory getMF() {
+		if (nMP==null) {
+			nMP = new MapaFactory();
+		}
+		return nMP;
+	}
+	
+	public Mapa mapaSortu() {
+		//TODO
+	}
+}
