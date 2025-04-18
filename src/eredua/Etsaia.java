83b1f372683ffccfa81b71b4835b898f3b7472d6
LUC1A05 <ldel-rio@student.42urduliz.com>
2025-04-18 12:57:25 +0200

    etsaia inteligentea (demasiado inteligente como para poder ganar)

--- src/eredua/Etsaia.java
+++ src/eredua/Etsaia.java
@@ -3,6 +3,9 @@ package eredua;
 import common.Mugimendu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import javax.swing.Timer;
 
@@ -28,36 +31,115 @@ public class Etsaia {
 	}
 	
 	private void mugitu() {
+		int bidea = this.biderikMotzena();
 		
-		ArrayList<Integer> mugimenduPosibleak = new ArrayList();
-		if (mugituAhal(this.yPos-1,this.xPos)) {
-			mugimenduPosibleak.add(1);
-		} 
-		if (mugituAhal(this.yPos+1,this.xPos)) {
-			mugimenduPosibleak.add(2);
+		if(bidea != -1) {
+			int x = bidea%10;
+			int y = bidea/10;
+			
+			if(x == this.xPos-1 && this.mugituAhal(y, x)) {
+				this.mugituEzkerra();
+			}
+			else if(x == this.xPos+1 && this.mugituAhal(y, x)) {
+				this.mugituEskuma();
+			}
+			else if(y == this.yPos-1 && this.mugituAhal(y, x)) {
+				this.mugituGora();
+			}
+			else if(y == this.yPos+1 && this.mugituAhal(y, x)) {
+				this.mugituBehera();
+			}
 		}
-		if (mugituAhal(this.yPos,this.xPos-1)) {
-			mugimenduPosibleak.add(3);
-		} 
-		if (mugituAhal(this.yPos,this.xPos+1)) {
-			mugimenduPosibleak.add(4);
-		}
-		mugimenduPosibleak.add(5);
-		int zenbRandom = Dadoa.getNireDadoa().zenbakiaAukeratu(1,mugimenduPosibleak.size()+1);
-		int zenb = mugimenduPosibleak.get(zenbRandom-1);
-		if (zenb == 1) {
-			mugituGora();
-		} else if (zenb == 2) {
-			mugituBehera();
-		} else if (zenb == 3) {
-			mugituEzkerra();
-		} else if (zenb == 4) {
-			mugituEskuma();
+		else {
+			ArrayList<Integer> mugimenduPosibleak = new ArrayList();
+			if (mugituAhal(this.yPos-1,this.xPos)) {
+				mugimenduPosibleak.add(1);
+			} 
+			if (mugituAhal(this.yPos+1,this.xPos)) {
+				mugimenduPosibleak.add(2);
+			}
+			if (mugituAhal(this.yPos,this.xPos-1)) {
+				mugimenduPosibleak.add(3);
+			} 
+			if (mugituAhal(this.yPos,this.xPos+1)) {
+				mugimenduPosibleak.add(4);
+			}
+			mugimenduPosibleak.add(5);
+			int zenbRandom = Dadoa.getNireDadoa().zenbakiaAukeratu(1,mugimenduPosibleak.size()+1);
+			int zenb = mugimenduPosibleak.get(zenbRandom-1);
+			if (zenb == 1) {
+				mugituGora();
+			} else if (zenb == 2) {
+				mugituBehera();
+			} else if (zenb == 3) {
+				mugituEzkerra();
+			} else if (zenb == 4) {
+				mugituEskuma();
+			}	
 		}
 		
-//		while (ahal == false) {
-//		}
 	}
+	
+	//smart
+	private int biderikMotzena() {
+		int etsaiPos = yPos*10+xPos;
+		int jokPos = JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getJokalaria().getY()*10 + JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getJokalaria().getX();
+		int pos = -1;
+		
+		Queue<Integer> aztertuGabeak = new LinkedList<Integer>();
+		HashMap<Integer, Integer> backPointers = new HashMap<Integer,Integer>();
+		ArrayList<Integer> bidea = new ArrayList<Integer>();
+		
+		boolean aurkitua = false;
+		aztertuGabeak.add(etsaiPos);
+		backPointers.put(etsaiPos, null);
+		
+		while(!aztertuGabeak.isEmpty() && !aurkitua) {
+			Integer unekoa = aztertuGabeak.peek();
+			int y = unekoa /10;
+			int x = unekoa %10;
+			if(JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(y, x).getJokalaria()!=null) {
+				aurkitua = true;
+			}
+			else {
+				int auk1 = (y-1)*10+x;
+				int auk2 = (y+1)*10+x;
+				int auk3 = y*10+x-1;
+				int auk4 = y*10+x+1;
+				if(!backPointers.containsKey(auk1) && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk1/10, auk1%10)!=null && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk1/10, auk1%10).hutsikDago() && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk1/10, auk1%10).getBonba()==null) {
+					backPointers.put(auk1, unekoa);
+					aztertuGabeak.add(auk1);
+				}
+				if(!backPointers.containsKey(auk2) && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk2/10, auk2%10)!=null && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk2/10, auk2%10).hutsikDago() && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk2/10, auk2%10).getBonba()==null) {
+					backPointers.put(auk2, unekoa);
+					aztertuGabeak.add(auk2);
+				}
+				if(!backPointers.containsKey(auk3) && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk3/10, auk3%10)!=null && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk3/10, auk3%10).hutsikDago() && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk3/10, auk3%10).getBonba()==null) {
+					backPointers.put(auk3, unekoa);
+					aztertuGabeak.add(auk3);
+				}
+				if(!backPointers.containsKey(auk4) && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk4/10, auk4%10)!=null && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk4/10, auk4%10).hutsikDago() && JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(auk4/10, auk4%10).getBonba()==null) {
+					backPointers.put(auk4, unekoa);
+					aztertuGabeak.add(auk4);
+				}
+				aztertuGabeak.remove();
+			}
+		}
+		if(aurkitua) {
+			int unekoa = aztertuGabeak.poll();
+			while(backPointers.get(unekoa) != null) {
+				bidea.add(0,unekoa);
+				unekoa = backPointers.get(unekoa);
+			}
+			//interesatzen zaigun pozizioa
+			if(!bidea.isEmpty()) {
+				pos = bidea.get(0);				
+			}
+		}
+		return pos;
+		
+	}
+	//smart
 	
 	private void mugituGora() {
 		JokoKudeatzailea.getJokoKudeatzaileaa().getEreduMapa().getGelaxka(yPos, xPos).setEtsaia(null);
