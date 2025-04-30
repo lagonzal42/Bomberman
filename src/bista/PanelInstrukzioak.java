303d20bb729556599e4d053af13d9dc9e0964d52
Ainhoa <ainhoatomas@gmail.com>
2025-04-30 11:27:15 +0100

    Ins Etsaiak

--- src/bista/PanelInstrukzioak.java
+++ src/bista/PanelInstrukzioak.java
@@ -6,7 +6,6 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class PanelInstrukzioak extends JPanel implements PanelEgoera{
-	private JPanel PanelIns;
 
 	private static final long serialVersionUID = 1L;
 
@@ -20,31 +19,37 @@ public class PanelInstrukzioak extends JPanel implements PanelEgoera{
 		
 		// TEKLAK
 		JLabel teklakLabel = new JLabel("<html><b>TEKLAK:</b><br>"
-		        + "B: bonba jarri<br>"
-		        + "P: bonba berezia jarri<br>"
-		        + "Geziak: mugitzeko</html>");
+		        + "B: bonba edo ultrabonda jarri.<br>"
+		        + "P: bonba berezia jarri.<br>"
+		        + "Geziak: mugitzeko.</html>");
 		teklakLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         
 		//PERTSONAIAK
 		JLabel pertsonaiakLabel = new JLabel("<html><b>PERTSONAIAK:</b><br>"
-		        + "Zuria: bonba normalak ditu<br>"
-		        + "Beltza: ultrabonba du<br>"
-		        + "Urdina: bi bonbak ditu</html>");
+		        + "Zuria: bonba normalak ditu eta berezi bat.<br>"
+		        + "Beltza: ultrabonba du eta berezi bat.<br>"
+		        + "Urdina: bietako bonba bat ager daiteke eta bonaba berezi bat du.</html>");
 		pertsonaiakLabel.setFont(new Font("Arial", Font.PLAIN, 16));
 		
 		// BONBAK
 		JLabel bonbakLabel = new JLabel("<html><b>BONBAK:</b><br>"
-		        + "Bonba normala: bere alboetan estanda egiten du<br>"
-		        + "Ultrabonba: dagoen fila osoan estanda egiten du<br>"
-		        + "Bonba berezia: alboan eta diagonaletan estanda egiten du, ez zaitu hiltzen eta bloke gogorrak <br> apurtzen ditu</html>");
+		        + "Bonba normala: bere alboetan estanda egiten du.<br>"
+		        + "Ultrabonba: dagoen fila osoan estanda egiten du.<br>"
+		        + "Bonba berezia: alboan eta diagonaletan estanda egiten du, ez zaitu hiltzen eta bloke gogorrak <br> apurtzen ditu.</html>");
 		bonbakLabel.setFont(new Font("Arial", Font.PLAIN, 16));
 		
 		// MAPAK
 		JLabel mapakLabel = new JLabel("<html><b>MAPAK:</b><br>"
-		        + "Classic: Bloke gogorrak eta bigunak dituen mapa<br>"
-		        + "Soft: Bloke bigunak dituen mapa<br>"
-		        + "Empty: Blokerik gabeko mapa</html>");
+		        + "Classic: Bloke gogorrak eta bigunak dituen mapa.<br>"
+		        + "Soft: Bloke bigunak dituen mapa.<br>"
+		        + "Empty: Blokerik gabeko mapa.</html>");
 		mapakLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+		
+		// MAPAK
+		JLabel etsaiakLabel = new JLabel("<html><b>ETSAIAK:</b><br>"
+		        + "Larrosa: Etsai normalak, errazago hiltzeko.<br>"
+		        + "Urdina: Etsai adimentsuak, jokalaria jarraitzen dute.</html>");
+		etsaiakLabel.setFont(new Font("Arial", Font.PLAIN, 16));
 		
 		//JLabel panelean sartu
 		panelIns.add(teklakLabel);
@@ -54,6 +59,8 @@ public class PanelInstrukzioak extends JPanel implements PanelEgoera{
 		panelIns.add(bonbakLabel);
 		panelIns.add(Box.createVerticalStrut(10));
 		panelIns.add(mapakLabel);
+		panelIns.add(Box.createVerticalStrut(10));
+		panelIns.add(etsaiakLabel);
 		panelIns.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		
 		add(panelIns, BorderLayout.WEST);
